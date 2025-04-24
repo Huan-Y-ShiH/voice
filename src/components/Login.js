dc6e63ef2b87c7d0922ebe4b10ea6f7d0d531c5a
Huan-Y-ShiH <18801039624@163.com>
2025-04-24 11:04:07 +0800

    Initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,7 +27,7 @@ function Login() {
 
     try {
       console.log('开始注册请求...');
-      const response = await fetch('https://www.srtp.site/register', {
+      const response = await fetch('https://www.srtp.site:8080/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
